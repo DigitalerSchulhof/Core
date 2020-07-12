f1b78f6325d10e4f07a252eeb5dbc43b76a6b5a1
Patrick Wagner <pawa@matinf.de>
2020-07-12 10:11:47 +0200

    [Modul] UI

--- core/funktionen.php
+++ core/funktionen.php
@@ -5,4 +5,24 @@ function style($css) {
 	return "<link rel=\"stylesheet\" href=\"$css?$cb\">";
 }
 
+function istZahl($x) {
+	if (preg_match("/^[0-9]+$/", $text)) {
+		return false;
+	}	else {
+		return true;
+	}
+}
+
+function fuehrendeNull($x) {
+	if (istZahl($x)) {
+		if (strlen($x) < 2) {
+			return "0".$x;
+		} else {
+			return $x;
+		}
+	} else {
+		return false;
+	}
+}
+
 ?>
