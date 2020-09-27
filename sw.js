355e3d27fef98a66e45020e64d1076af83bdb52e
jeengbe <jesper.engberg@gmx.at>
2020-09-27 16:19:49 +0200

    [Neu] Seite Offline verfügbar

--- sw.js
+++ sw.js
@@ -4,23 +4,23 @@ self.addEventListener("activate", (event) => {
   // caches.delete(cacheName);
 });
 self.addEventListener("fetch", function (event) {
-  if (event.request.method === "GET" && event.request.url.indexOf("http") === 0) {
+  if (
+    event.request.method === "GET" &&
+    event.request.url.indexOf("http") === 0
+  ) {
     event.respondWith(
-      caches.open(cacheName).then(function (cache) {
-        return cache.match(event.request).then(function (response) {
-          if (response) {
-            fetch(event.request).then(function (response) {
-              cache.put(event.request, response.clone());
-              return response;
-            });
-          }
-          return (
-            response ||
-            fetch(event.request).then(function (response) {
-              cache.put(event.request, response.clone());
-              return response;
-            })
-          );
+      caches.open(cacheName).then(async function (cache) {
+        const response = await cache.match(event.request);
+        if (response) {
+          fetch(event.request).then(function (response_1) {
+            cache.put(event.request, response_1.clone());
+            return response_1;
+          });
+          return response;
+        }
+        return fetch(event.request).then(function (response_2) {
+          cache.put(event.request, response_2.clone());
+          return response_2;
         });
       })
     );
