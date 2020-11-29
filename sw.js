d89ad785dab4c4263082cafc86fbc0655b649271
jeengbe <jesper.engberg@gmx.at>
2020-11-29 09:06:29 +0100

    [Neu] ESLint - [Neu] TypeScript Compiler

--- sw.js
+++ sw.js
@@ -1,13 +1,10 @@
 const cacheName = "DigitalerSchulhof";
-self.addEventListener("install", function (event) {});
-self.addEventListener("activate", (event) => {
+self.addEventListener("install", _ => {});
+self.addEventListener("activate", _ => {
   // caches.delete(cacheName);
 });
 self.addEventListener("fetch", function (event) {
-  if (
-    event.request.method === "GET" &&
-    event.request.url.indexOf("http") === 0
-  ) {
+  if (event.request.method === "GET" && event.request.url.indexOf("http") === 0) {
     event.respondWith(
       caches.open(cacheName).then(async function (cache) {
         const response = await cache.match(event.request);
